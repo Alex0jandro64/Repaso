28e88c6dd66536f667af7d099a03ec378f96ea74
Alex0jandro64 <alejandroalcerreca3@gmail.com>
2023-09-20 09:25:16 +0200

    Initial commit

---
+++ Entidades/Empleado.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso.Modelos
+{
+    internal class Empleado
+    {
+        //Atributos
+        string nombre, apellidos, dni, fechaNacimiento, titulacion;
+        int numSeguridadSocial, numCuenta;
+
+        //Constructores
+        public Empleado()
+        {
+        }
+
+        public Empleado(string nombre, string apellidos, string dni, string fechaNacimiento, string titulacion, int numSeguridadSocial, int numCuenta)
+        {
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.dni = dni;
+            this.fechaNacimiento = fechaNacimiento;
+            this.titulacion = titulacion;
+            this.numSeguridadSocial = numSeguridadSocial;
+            this.numCuenta = numCuenta;
+        }
+
+        //Getters y Setters
+
+    }
+}
---
+++ Servicios/ImplMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso.Servicios
+{
+    internal class ImplMenu:InterfazMenu
+    {
+        public void Menu()
+        {
+            Console.WriteLine("1.-Registro Empleado");
+            Console.WriteLine("2.-Modificar Empleado");
+            Console.WriteLine("3.-Exportar a Fichero");
+            Console.WriteLine("4.-Cerrar");
+
+        }
+    }
+}
